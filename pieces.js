7a23795d7badc096d80bf358c76f954ce0110f10
merveille18 <107496563+merveille18@users.noreply.github.com>
2023-02-01 18:57:40 +0100

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -11,6 +11,11 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite = true "En stock" : "Rupture de stock";
+
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
